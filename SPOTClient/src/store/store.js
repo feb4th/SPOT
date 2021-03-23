9207c812435ae2085a1bf363b83f7ff8b3da373a
username <sis6617@naver.com>
2021-03-23 15:22:50 +0900

    [S04P23A102-10] INIT : 프론트엔드_로그인 구성

--- SPOTClient/src/store/store.js
+++ SPOTClient/src/store/store.js
@@ -2,15 +2,14 @@ import Vue from "vue";
 import Vuex from "vuex";
 
 //store modeule import
-import ExampleStore from './modules/ExampleStore';
-
+import ExampleStore from "./modules/ExampleStore";
+import MemberStore from "./modules/MemberStore";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   modules: {
-    ExampleStore
+    ExampleStore,
+    MemberStore
   },
-  plugins: [
-    
-  ]
+  plugins: []
 });
