945a688ff49999373596597f3d70a8b712386f73
김영현 <kimyh03160@naver.com>
2021-03-23 07:59:22 +0000

    [S04P23A102-16] INIT : 프론트엔드_검색 페이지 구성

--- SPOTClient/src/components/Find/SearchBar.vue
+++ SPOTClient/src/components/Find/SearchBar.vue
@@ -1,7 +1,40 @@
-<template> </template>
+<template>
+  <v-container>
+    <v-row justify="space-around">
+      <v-col cols="6" align="center">
+        <v-text-field
+          v-model="msg"
+          label="검색어"
+          placeholder="원하는 검색어를 입력하세요."
+          append-outer-icon="mdi-magnify"
+          @keypress.enter="onSearch"
+          @click:append-outer="onSearch"
+          clear-icon="mdi-eraser"
+          clearable
+          @click:clear="onReset"
+        ></v-text-field>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
 
 <script>
-	export default {};
+export default {
+  data() {
+    return {
+      msg: ""
+    };
+  },
+  methods: {
+    onSearch() {
+      //여기서 검색을 하고, 검색 대상을 Store에 선언해둔 녀석에 넣는건가?
+      this.msg = "";
+    },
+    onReset() {
+      this.msg = "";
+    }
+  }
+};
 </script>
 
 <style></style>
--- SPOTClient/src/views/Find.vue
+++ SPOTClient/src/views/Find.vue
@@ -1,7 +1,64 @@
-<template> </template>
+<template>
+  <v-app id="inspire">
+    <v-app-bar app color="white" flat>
+      <v-avatar
+        :color="$vuetify.breakpoint.smAndDown ? 'grey darken-1' : 'transparent'"
+        size="32"
+      ></v-avatar>
+
+      <v-avatar
+        class="hidden-sm-and-down"
+        color="grey darken-1 shrink"
+        size="32"
+      ></v-avatar>
+    </v-app-bar>
+
+    <v-main class="grey lighten-3">
+      <v-container>
+        <v-row>
+          <v-col cols="12" sm="2">
+            <v-sheet rounded="lg" min-height="268">
+              <!--  -->
+            </v-sheet>
+          </v-col>
+
+          <v-col cols="12" sm="8">
+            <v-sheet min-height="70vh" rounded="lg">
+              <!--  -->
+              <search-bar />
+              <suggest-list v-if="listFlag" />
+              <search-list v-else />
+            </v-sheet>
+          </v-col>
+
+          <v-col cols="12" sm="2">
+            <v-sheet rounded="lg" min-height="268">
+              <!--  -->
+            </v-sheet>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
 
 <script>
-	export default {};
+import SearchBar from "../components/Find/SearchBar.vue";
+import SearchList from "../components/Find/SearchList.vue";
+import SuggestList from "../components/Find/SuggestList.vue";
+
+export default {
+  data() {
+    return {
+      listFlag: false
+    };
+  },
+  components: {
+    SearchBar,
+    SearchList,
+    SuggestList
+  }
+};
 </script>
 
-<style></style>
+<style scoped></style>
