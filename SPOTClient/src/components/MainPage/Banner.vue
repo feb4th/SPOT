a0caab7ab133efc1d249eea760f7bce8a044b784
최나현 <nahyunee24@naver.com>
2021-04-06 20:42:26 +0900

    [S04P23A102-97] ADD : 로그아웃 구현
    - 메인페이지에 로그아웃 버튼 구현
    - store에 있는 actions를 통해 session storage remove 한 뒤, login
      페이지로 이동

--- SPOTClient/src/components/MainPage/Banner.vue
+++ SPOTClient/src/components/MainPage/Banner.vue
@@ -1,24 +1,37 @@
 <template>
   <v-container>
     <v-row class="mx-4">
-      <v-col clas="mt-2" align="end">
+      <v-col clas="mt-2">
         <p style="font-size : 2.5em;">
           {{ getMemberInfo.nickname }}
           <span style="font-size : 20px; margin-left: -10px;">님,</span>
         </p>
-        <p style="font-size: 2.5em;">환영합니다!</p>
+        <p style="font-size: 2.5em;">
+          환영합니다!
+          <v-btn depressed icon @click="logout"
+            ><v-icon>mdi-logout</v-icon></v-btn
+          >
+        </p>
       </v-col>
     </v-row>
   </v-container>
 </template>
 
 <script>
-import { mapGetters } from "vuex";
+import { mapGetters, mapActions } from "vuex";
 const MemberStore = "MemberStore";
 
 export default {
   computed: {
     ...mapGetters(MemberStore, ["getMemberInfo"])
+  },
+  methods: {
+    ...mapActions(MemberStore, ["Logout"]),
+
+    logout() {
+      this.Logout();
+      this.$router.push("/login");
+    }
   }
 };
 </script>
