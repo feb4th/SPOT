311e5ee4dcee1f4b664c1a844e6f1432ee18edf3
shin <sis6617@naver.com>
2021-04-08 09:34:20 +0900

    ADD : 프론트엔드_기능 확인

--- SPOTClient/src/views/Map.vue
+++ SPOTClient/src/views/Map.vue
@@ -2,12 +2,13 @@
   <div id="map-wrapper" class="map-wrapper">
     <div v-if="province" class="province-title text-left">
       <h3 class="text-center">
-        {{province.SGG_2}}
+        {{ province.SGG_2 }}
         <br />
         <h4>
           <template v-if="province.candidate">
             <strong>
-            {{ partyList[province.candidate.party].name }}, {{ province.candidate.name }}
+              {{ partyList[province.candidate.party].name }},
+              {{ province.candidate.name }}
             </strong>
           </template>
         </h4>
@@ -19,12 +20,13 @@
 
     <div v-if="currentProvince" class="province-info">
       <h3 class="text-center">
-        {{currentProvince.SGG_2}}
+        {{ currentProvince.SGG_2 }}
         <br />
         <h4>
           <template v-if="currentProvince.candidate">
             <strong>
-            {{ partyList[currentProvince.candidate.party].name }}, {{ currentProvince.candidate.name }}
+              {{ partyList[currentProvince.candidate.party].name }},
+              {{ currentProvince.candidate.name }}
             </strong>
           </template>
         </h4>
@@ -41,19 +43,19 @@
         <li>SGG_2: {{currentProvince.SGG_3}}</li>
         -->
         <template v-if="currentProvince.final">
-        <li> 
-          <b-row>
-            <b-col class="col-12 text-white">
-              개표결과
-            </b-col>
-            <b-col class="col-12 text-white">
-              <vote21-ratio-bar
-                :party-list="partyList"
-                :data="currentProvince.final"
-              ></vote21-ratio-bar>
-            </b-col>
-          </b-row>
-        </li>
+          <li>
+            <b-row>
+              <b-col class="col-12 text-white">
+                개표결과
+              </b-col>
+              <b-col class="col-12 text-white">
+                <vote21-ratio-bar
+                  :party-list="partyList"
+                  :data="currentProvince.final"
+                ></vote21-ratio-bar>
+              </b-col>
+            </b-row>
+          </li>
         </template>
       </ul>
     </div>
@@ -62,57 +64,141 @@
       <h4 class="text-center">정당별 현황</h4>
       <ul>
         <template v-for="(value, key) in localSeatData">
-          <li :key="key">{{ partyList[key].name }} : {{value | digit }} 석</li>
+          <li :key="key">{{ partyList[key].name }} : {{ value | digit }} 석</li>
         </template>
       </ul>
     </div>
-
   </div>
 </template>
 
 <script>
-import * as d3 from 'd3'
+import * as d3 from "d3";
 
-const MAP_GEOJSON = require('./map.geo.json');
-const MAP_AREA = require('./map.area.json');
-const LOCAL_SEAT_FINAL = require('./localseat.final').default;
+const MAP_GEOJSON = require("./map.geo.json");
+const MAP_AREA = require("./map.area.json");
+const LOCAL_SEAT_FINAL = require("./localseat.final").default;
 
-const Vote21RatioBar = require('./Vote21.RatioBar').default;
+const Vote21RatioBar = require("./Vote21.RatioBar").default;
 export default {
   components: {
-    "vote21-ratio-bar" : Vote21RatioBar,
+    "vote21-ratio-bar": Vote21RatioBar
   },
-  props: {
-  },
+  props: {},
   data() {
     return {
-      province: undefined,            // 마우스가 지역구 위에 있을 때 정보
-      currentProvince: undefined,     // 지역구를 클릭했을 때 정보
-      mapArea: MAP_AREA,              // 지역구 지역정보
+      province: undefined, // 마우스가 지역구 위에 있을 때 정보
+      currentProvince: undefined, // 지역구를 클릭했을 때 정보
+      mapArea: MAP_AREA, // 지역구 지역정보
 
-      localSeatData : {},             // 정당별 당선자수 요약
-      partyList : {
-        theminjoo : {name: "더불어민주당", no:1, localLock:false, ratioLock:true, color:"#004EA2"},
-        unitedfuture : {name: "미래통합당", no:2, localLock:false, ratioLock:true, color:"#EF426F"},
-        minsaengdang : {name: "민생당", no:3, localLock:false, ratioLock:false, color:"#0BA95F"},
-        futurekorea : {name: "미래한국당", no:4, localLock:true, ratioLock:false, color:"#EF426F"},
-        platformparty : {name: "더불어시민당", no:5, localLock:true, ratioLock:false, color:"#0088D2"},
-        justice21 : {name: "정의당", no:6, localLock:false, ratioLock:false, color:"#FFCA05"},
-        ourrepublican : {name: "우리공화당", no:7, localLock:false, ratioLock:false, color:"#009900"},
-        minjung : {name: "민중당", no:8, localLock:false, ratioLock:false, color:"#F47920"},
-        koreaeconomy : {name: "한국경제당", no:9, localLock:true, ratioLock:false, color:"#009900"},
-        people21 : {name: "국민의당", no:10, localLock:true, ratioLock:false, color:"#EA5504"},
-        proparknew : {name: "친박신당", no:11, localLock:false, ratioLock:false, color:"#BBBBBB"},
-        openminjoo : {name: "열린민주당", no:12, localLock:true, ratioLock:false, color:"#004098"},
-        etcparty : {name: "기타", localLock:false, ratioLock:false, color:"#BBBBBB"},
-        noparty : {name: "무소속", localLock:false, ratioLock:true, color:"#AAAAAA"},
-        undecided : {name: "무당층", localLock:true, ratioLock:false, color:"#CCCCCC"},
+      localSeatData: {}, // 정당별 당선자수 요약
+      partyList: {
+        theminjoo: {
+          name: "더불어민주당",
+          no: 1,
+          localLock: false,
+          ratioLock: true,
+          color: "#004EA2"
+        },
+        unitedfuture: {
+          name: "미래통합당",
+          no: 2,
+          localLock: false,
+          ratioLock: true,
+          color: "#EF426F"
+        },
+        minsaengdang: {
+          name: "민생당",
+          no: 3,
+          localLock: false,
+          ratioLock: false,
+          color: "#0BA95F"
+        },
+        futurekorea: {
+          name: "미래한국당",
+          no: 4,
+          localLock: true,
+          ratioLock: false,
+          color: "#EF426F"
+        },
+        platformparty: {
+          name: "더불어시민당",
+          no: 5,
+          localLock: true,
+          ratioLock: false,
+          color: "#0088D2"
+        },
+        justice21: {
+          name: "정의당",
+          no: 6,
+          localLock: false,
+          ratioLock: false,
+          color: "#FFCA05"
+        },
+        ourrepublican: {
+          name: "우리공화당",
+          no: 7,
+          localLock: false,
+          ratioLock: false,
+          color: "#009900"
+        },
+        minjung: {
+          name: "민중당",
+          no: 8,
+          localLock: false,
+          ratioLock: false,
+          color: "#F47920"
+        },
+        koreaeconomy: {
+          name: "한국경제당",
+          no: 9,
+          localLock: true,
+          ratioLock: false,
+          color: "#009900"
+        },
+        people21: {
+          name: "국민의당",
+          no: 10,
+          localLock: true,
+          ratioLock: false,
+          color: "#EA5504"
+        },
+        proparknew: {
+          name: "친박신당",
+          no: 11,
+          localLock: false,
+          ratioLock: false,
+          color: "#BBBBBB"
+        },
+        openminjoo: {
+          name: "열린민주당",
+          no: 12,
+          localLock: true,
+          ratioLock: false,
+          color: "#004098"
+        },
+        etcparty: {
+          name: "기타",
+          localLock: false,
+          ratioLock: false,
+          color: "#BBBBBB"
+        },
+        noparty: {
+          name: "무소속",
+          localLock: false,
+          ratioLock: true,
+          color: "#AAAAAA"
+        },
+        undecided: {
+          name: "무당층",
+          localLock: true,
+          ratioLock: false,
+          color: "#CCCCCC"
+        }
       },
-      localSeatFinal : LOCAL_SEAT_FINAL,
-    }
+      localSeatFinal: LOCAL_SEAT_FINAL
+    };
   },
-  computed: {
-  },
+  computed: {},
   created() {
     this.summarize();
   },
@@ -127,14 +213,14 @@ export default {
       Object.keys(this.localSeatFinal).forEach(localSeatCode => {
         let candidate = null;
         const localSeat = this.localSeatFinal[localSeatCode];
-        localSeat.some((item) => {
-          if(item.elected === true) {
+        localSeat.some(item => {
+          if (item.elected === true) {
             candidate = item;
             return true;
           }
         });
-        if(candidate) {
-          if(candidate.party in this.localSeatData) {
+        if (candidate) {
+          if (candidate.party in this.localSeatData) {
             this.localSeatData[candidate.party]++;
           } else {
             this.localSeatData[candidate.party] = 1;
@@ -146,8 +232,11 @@ export default {
     // 지역구의 지역 정보 찾기
     findArea(code) {
       // console.log(code);
-      if(code in this.mapArea) {
-        return Array.isArray(this.mapArea[code]) && this.mapArea[code].length >= 2 ? this.mapArea[code][1] : "";
+      if (code in this.mapArea) {
+        return Array.isArray(this.mapArea[code]) &&
+          this.mapArea[code].length >= 2
+          ? this.mapArea[code][1]
+          : "";
       }
       return "";
     },
@@ -155,8 +244,8 @@ export default {
     findCandidate(code) {
       let candidate = null;
       const localSeat = this.localSeatFinal[code];
-      localSeat.some((item) => {
-        if(item.elected === true) {
+      localSeat.some(item => {
+        if (item.elected === true) {
           candidate = item;
           return true;
         }
@@ -166,7 +255,7 @@ export default {
     },
     // 선택된 지역구
     selectProvince(province) {
-      if(province) {
+      if (province) {
         // console.log(province);
         // console.log(this.findCandidate(province.SGG_Code));
         province.candidate = this.findCandidate(province.SGG_Code);
@@ -177,7 +266,7 @@ export default {
     // 지역구 정보 열기
     openInfo(province) {
       // console.log(province);
-      if(province) {
+      if (province) {
         province.candidate = this.findCandidate(province.SGG_Code);
         province.final = this.localSeatFinal[province.SGG_Code];
       }
@@ -191,23 +280,22 @@ export default {
     partyColor(code) {
       let color = null;
       const localSeatCode = "" + code;
-      if(localSeatCode in this.localSeatFinal) {
+      if (localSeatCode in this.localSeatFinal) {
         const localSeat = this.localSeatFinal[localSeatCode];
         let candidate = null;
-        localSeat.some((item) => {
-          if(item.elected === true) {
+        localSeat.some(item => {
+          if (item.elected === true) {
             candidate = item;
             return true;
           }
         });
-        if(candidate) {
+        if (candidate) {
           const party = candidate.party;
           color = this.partyList[party].color;
         }
       }
       return color;
     },
-
 
     // https://codepen.io/lantoine/pen/ExxEGxg
     // https://m.blog.naver.com/PostView.nhn?blogId=jhc9639&logNo=221195987416&proxyReferer=https%3A%2F%2Fwww.google.com%2F
@@ -221,61 +309,66 @@ export default {
 
       // 현재의 브라우저의 크기 계산
       const divWidth = document.getElementById("map-wrapper").clientWidth;
-      const width = (divWidth < 1000) ? divWidth * 0.9 : 1000;
+      const width = divWidth < 1000 ? divWidth * 0.9 : 1000;
       const height = width * 1;
 
       // 지도를 그리기 위한 svg 생성
       const svg = d3
-      // .select('.d3')
-        .select('.map-wrapper')
-        .append('svg')
-        .attr('width', width)
-        .attr('height', height);
+        // .select('.d3')
+        .select(".map-wrapper")
+        .append("svg")
+        .attr("width", width)
+        .attr("height", height);
 
       // 배경 그리기
-      const background = svg.append('rect')
-        .attr('class', 'background')
-        .attr('width', width)
-        .attr('height', height)
+      const background = svg
+        .append("rect")
+        .attr("class", "background")
+        .attr("width", width)
+        .attr("height", height);
 
       // 지도가 그려지는 그래픽 노드(g) 생성
-      const g = svg.append('g');
+      const g = svg.append("g");
       // const effectLayer = g.append('g').classed('effect-layer', true);
       // 지도가 그려지는 그래픽 노드
-      const mapLayer = g.append('g').classed('map-layer', true);
+      const mapLayer = g.append("g").classed("map-layer", true);
       // 아이콘이 그려지는 그래픽 노드
-      const iconsLayer = g.append('g').classed('icons-layer', true);
+      const iconsLayer = g.append("g").classed("icons-layer", true);
 
       // 지도의 출력 방법을 선택(메로카토르)
-      let projection = d3.geoMercator()
+      let projection = d3
+        .geoMercator()
         .scale(1)
-        .translate([0, 0]); 
+        .translate([0, 0]);
 
       // svg 그림의 크기에 따라 출력될 지도의 크기를 다시 계산
       const path = d3.geoPath().projection(projection);
       const bounds = path.bounds(geojson);
-      const widthScale = (bounds[1][0] - bounds[0][0]) / width; 
-      const heightScale = (bounds[1][1] - bounds[0][1]) / height; 
+      const widthScale = (bounds[1][0] - bounds[0][0]) / width;
+      const heightScale = (bounds[1][1] - bounds[0][1]) / height;
       const scale = 0.95 / Math.max(widthScale, heightScale);
-      const xoffset = width/2 - scale * (bounds[1][0] + bounds[0][0]) /2 + 0; 
-      const yoffset = height/2 - scale * (bounds[1][1] + bounds[0][1])/2 + 0; 
+      const xoffset =
+        width / 2 - (scale * (bounds[1][0] + bounds[0][0])) / 2 + 0;
+      const yoffset =
+        height / 2 - (scale * (bounds[1][1] + bounds[0][1])) / 2 + 0;
       const offset = [xoffset, yoffset];
       projection.scale(scale).translate(offset);
 
-      const color = d3.scaleLinear()
+      const color = d3
+        .scaleLinear()
         .domain([1, 20])
         .clamp(true)
         // .range(['#08304b', '#08304b']);
-        .range(['#595959', '#595959']);
+        .range(["#595959", "#595959"]);
 
       const _this = this;
       // Get province color
-      function fillFn(d){
+      function fillFn(d) {
         // console.log(d, nameLength(d));
         // console.log(d.properties);
 
         const pcolor = _this.partyColor(d.properties.SGG_Code);
-        if(pcolor) {
+        if (pcolor) {
           return pcolor;
         }
 
@@ -302,74 +395,84 @@ export default {
         }
 
         // Highlight the clicked province
-        mapLayer.selectAll('path')
-          .style('fill', function(d){
-            return centered && d===centered ? '#D5708B' : fillFn(d);
+        mapLayer.selectAll("path").style("fill", function(d) {
+          return centered && d === centered ? "#D5708B" : fillFn(d);
         });
 
         // Zoom
         g.transition()
           .duration(750)
-          .attr('transform', 'translate(' + width / 2 + ',' + height / 2 + ')scale(' + k + ')translate(' + -x + ',' + -y + ')');
+          .attr(
+            "transform",
+            "translate(" +
+              width / 2 +
+              "," +
+              height / 2 +
+              ")scale(" +
+              k +
+              ")translate(" +
+              -x +
+              "," +
+              -y +
+              ")"
+          );
       }
 
-
-      function mouseover(d){
+      function mouseover(d) {
         // Highlight hovered province
-        d3.select(this).style('fill', '#1483ce');
+        d3.select(this).style("fill", "#1483ce");
         // d3.select(this).style('fill', '#004EA2');
-        if(d) {
+        if (d) {
           // console.log(d.properties);
           _this.selectProvince(d.properties);
         }
       }
 
-      function mouseout(d){
+      function mouseout(d) {
         _this.selectProvince(undefined);
         // Reset province color
-        mapLayer.selectAll('path')
-          .style('fill', (d) => {
-            return centered && d===centered ? '#D5708B' : fillFn(d);
-          });
+        mapLayer.selectAll("path").style("fill", d => {
+          return centered && d === centered ? "#D5708B" : fillFn(d);
+        });
       }
 
       // Get province name length
-      function nameLength(d){
+      function nameLength(d) {
         const n = nameFn(d);
         return n ? n.length : 0;
       }
 
       // Get province name
-      function nameFn(d){
+      function nameFn(d) {
         return d && d.properties ? d.properties.name : null;
       }
 
       // Add background
-      background
-        .on('click', clicked);
+      background.on("click", clicked);
 
       // 지도 그리기
       mapLayer
-        .selectAll('path')
+        .selectAll("path")
         .data(geojson.features)
-        .enter().append('path') 
-        .attr('d', path)
-        .attr('vector-effect', 'non-scaling-stroke')
-        .style('fill', fillFn)
-        .on('mouseover', mouseover)
-        .on('mouseout', mouseout)
-        .on('click', clicked);
+        .enter()
+        .append("path")
+        .attr("d", path)
+        .attr("vector-effect", "non-scaling-stroke")
+        .style("fill", fillFn)
+        .on("mouseover", mouseover)
+        .on("mouseout", mouseout)
+        .on("click", clicked);
 
       const iconsInfo = [
         {
-          "name":"서울",
-          "lat" : "37.532600",
-          "lon" : "126.904612"
+          name: "서울",
+          lat: "37.532600",
+          lon: "126.904612"
         },
         {
-          "name":"대전",
-          "lat" : "36.3730178",
-          "lon" : "127.2483736"
+          name: "대전",
+          lat: "36.3730178",
+          lon: "127.2483736"
         }
       ];
 
@@ -377,14 +480,14 @@ export default {
       // iconsLayer
       //   .selectAll('svg')
       //   .data(iconsInfo)
-      //   .enter()  
+      //   .enter()
       //   .append("svg:image")
       //   .attr("width", 30)
       //   .attr("height", 30)
       //   .attr('x',  d=> projection([d.lon, d.lat])[0])
       //   .attr('y',  d=> projection([d.lon, d.lat])[1])
       //   .attr('opacity', 0)
-      //   .attr("xlink:href", require("./medal.svg")) 
+      //   .attr("xlink:href", require("./medal.svg"))
       //   .on('click', d => {
       //     console.log(d)
       //   })
@@ -396,21 +499,23 @@ export default {
       //   .attr('y',  d=> projection([d.lon, d.lat])[1])
 
       // 확대/축소 이벤트 처리
-      const zoomed = () =>{
-        mapLayer.attr('transform', d3.event.transform)
-        iconsLayer.attr('transform', d3.event.transform)
-      }
-      const zoom = d3.zoom().scaleExtent([1, 8]).on('zoom', zoomed)
-      svg.call(zoom)
+      const zoomed = () => {
+        mapLayer.attr("transform", d3.event.transform);
+        iconsLayer.attr("transform", d3.event.transform);
+      };
+      const zoom = d3
+        .zoom()
+        .scaleExtent([1, 8])
+        .on("zoom", zoomed);
+      svg.call(zoom);
     }
   }
-}
-
+};
 </script>
 
 <style lang="scss">
 .map-wrapper {
-  position:relative;
+  position: relative;
   text-align: center;
 
   // svg {
@@ -459,7 +564,8 @@ export default {
     z-index: 103;
     pointer-events: none;
   }
-  .province-state > ul, .province-summary > ul {
+  .province-state > ul,
+  .province-summary > ul {
     list-style: none;
   }
   .background {
